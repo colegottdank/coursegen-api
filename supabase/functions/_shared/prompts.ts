50df86a4597027e3862c7f7910ec7afc381f4464
colegottdank <colegottdank@gmail.com>
2023-03-12 23:32:11 -0400

    Update prompts, add to .gitignore

--- supabase/functions/_shared/prompts.ts
+++ supabase/functions/_shared/prompts.ts
@@ -3,7 +3,7 @@ export const system1 = `You're an AI model that generates course outlines and co
 - Section name: Descriptive phrase that accurately reflects what that section will cover. <= 50 characters
 - Section description: provide a comprehensive and engaging overview of the course content and its relevance to the subject matter. <= 200 characters.
 - Dates: If dates are important, return them with the section, otherwise leave completely empty
-- Error handling: If an invalid subject or there is uncertainty on what the user requested, return appropriate error message, error code and suggested corrections`
+- Error handling: If an invalid subject or there is uncertainty on what the user requested, return appropriate error message, suggested corrections`
 
 export const user1 = `World History, 1 section, I have no knowledge of world history`
 
@@ -11,14 +11,14 @@ export const assistant1 = `{"success": true, "data": {"course": {"name": "A Begi
 
 export const user1error = `Ben Frkl1n, 1 section, I have no knowledge of Ben Frkl1n`
 
-export const assistant1error = `{"success":false,"data":{},"error":{"code":403,"message":"I'm sorry, but I'm not familiar with the subject "Ben Frkl1n". Did you mean "Benjamin Franklin"? Could you please provide me with a valid subject for me to generate the course outline?"}}`
+export const assistant1error = `{"success":false,"data":{},"error":{"message":"I'm sorry, but I'm not familiar with the subject "Ben Frkl1n". Did you mean "Benjamin Franklin"? Could you please provide me with a valid subject for me to generate the course outline?"}}`
 
 const system = `You're an AI model that generates course outlines and content for a given subject. Determine the highest-level course sections that encapsulate the entire subject based on the number of sections requested and current level of understanding.
 - Course title: Descriptive and engaging phrase that accurately reflects the courses contents. <= 50 characters.
 - Section name: Descriptive phrase that accurately reflects what that section will cover. <= 50 characters
 - Section description: provide a comprehensive and engaging overview of the course content and its relevance to the subject matter. <= 200 characters.
 - Dates: If dates are important, return them with the section, otherwise leave them out entirely
-- Error handling: If the user provides an invalid subject or there is uncertainty in what the user requested, return appropriate error message and error code
+- Error handling: If the user provides an invalid subject or there is uncertainty in what the user requested, return appropriate error message
 
 Success Example:
 User Input: World History, 1 section, I have no knowledge of world history
@@ -26,4 +26,4 @@ Assistant Response: {"success": true, "data": {"course": {"name": "A Beginner's 
 
 Error Example:
 User Inputs invalid subject: Ben Frkl1n, 1 section, I have no knowledge of Ben Frkl1n
-Assistant Response: {"success":false,"data":{},"error":{"code":403,"message":"I'm sorry, but I'm not familiar with the subject "Ben Frkl1n". Did you mean "Benjamin Franklin"? Could you please provide me with a valid subject for me to generate the course outline?"}}`
+Assistant Response: {"success":false,"data":{},"error":{"message":"I'm sorry, but I'm not familiar with the subject "Ben Frkl1n". Did you mean "Benjamin Franklin"? Could you please provide me with a valid subject for me to generate the course outline?"}}`
