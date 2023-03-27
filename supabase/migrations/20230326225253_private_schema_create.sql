d352ddb6e0dda973bca6544d66d11cf5f2d29f2c
colegottdank <colegottdank@gmail.com>
2023-03-26 23:42:28 -0400

    Fix remote

--- supabase/migrations/20230326225253_private_schema_create.sql
+++ supabase/migrations/20230326225253_private_schema_create.sql
@@ -1,8 +1,6 @@
 ALTER FUNCTION insert_course_and_sections
 SET SCHEMA public;
 
-drop policy "Individual insert access" on "public"."profile";
-
 drop policy "Users can update own profile." on "public"."profile";
 
 drop policy "Authenticated users can insert new course" on "public"."course";
