adc4268c6e2dd4512580eb1f906f808195ce4219
colegottdank <colegottdank@gmail.com>
2023-04-29 18:24:47 -0400

    Update course rls policies

---
+++ supabase/migrations/20230429214327_course_rls_update.sql
@@ -0,0 +1,43 @@
+-- Drop the existing policy
+DROP POLICY "Users can view own course" ON public.course;
+
+CREATE POLICY course_select_policy
+ON public.course
+FOR SELECT
+USING (true);
+
+CREATE OR REPLACE FUNCTION public.validate_course_update()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $function$
+BEGIN
+   IF NEW.id <> OLD.id THEN
+    RAISE EXCEPTION 'Updating "id" is not allowed';
+  END IF;
+
+  IF NEW.user_id <> OLD.user_id THEN
+    RAISE EXCEPTION 'Updating "user_id" is not allowed';
+  END IF;
+
+  IF NEW.search_text <> OLD.search_text THEN
+    RAISE EXCEPTION 'Updating "search_text" is not allowed';
+  END IF;
+
+  IF NEW.created_at <> OLD.created_at THEN
+    RAISE EXCEPTION 'Updating "created_at" is not allowed';
+  END IF;
+
+  IF NEW.updated_at <> OLD.updated_at THEN
+    RAISE EXCEPTION 'Updating "updated_at" is not allowed';
+  END IF;
+
+  RETURN NEW;
+END;
+$function$;
+
+-- Create the trigger
+CREATE TRIGGER before_course_update
+BEFORE UPDATE ON public.course
+FOR EACH ROW
+WHEN (row_security_active('course'))
+EXECUTE FUNCTION public.validate_course_update();
