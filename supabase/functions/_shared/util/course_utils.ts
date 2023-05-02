ef5c2fa523aef3618341760473fa980c87a7ee0c
colegottdank <colegottdank@gmail.com>
2023-05-01 21:02:41 -0400

    Fix course_utils

---
+++ supabase/functions/_shared/util/course_utils.ts
@@ -0,0 +1,12 @@
+import { Database } from "../database.types.ts";
+import { ICourse } from "../models/internal/ICourse.ts";
+
+export const mapCourseFromDb = (row: Database["public"]["Tables"]["course"]["Row"]): ICourse => {
+    return {
+      id: row.id,
+      title: row.title,
+      description: row.description,
+      dates: row.dates ?? undefined,
+      userId: row.user_id ?? undefined,
+    };
+  };
