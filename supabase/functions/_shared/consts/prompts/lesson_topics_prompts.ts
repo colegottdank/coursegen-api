6cca585e3d917b0df2f5a91a10c2435c17e9e1e0
colegottdank <colegottdank@gmail.com>
2023-05-30 18:41:35 -0400

    Improve emoji prompt

--- supabase/functions/_shared/consts/prompts/lesson_topics_prompts.ts
+++ supabase/functions/_shared/consts/prompts/lesson_topics_prompts.ts
@@ -117,7 +117,7 @@ Validate the lesson content against the course outline to ensure no repetition o
 Check for grammatical accuracy and coherence of ideas.
 Check the length of the content is greater than 2000 words if not, add more content.
 Add any markdown formatting to the content if it improves the readability of the content. This includes, tables, lists, bold, italics, etc.
-Add emojis to the topic titles that match the topic.
+Add emojis to the topic titles that match the topic (do not add to the topic content)
 Ensure the length of content per topic is not too short. If so, add more content or merge topics together.
 Remove all introduction and conclusion topics and replace with more content or additional topics if necessary.
 Preserve the response structure used previously. That is, valid refined lesson content should be in this format:
