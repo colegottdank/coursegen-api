aeae54c3a67c99e3fc542a2d70a3bf630e75b5c9
colegottdank <colegottdank@gmail.com>
2023-05-30 16:05:42 -0400

    Update improve lesson content prompt

--- supabase/functions/_shared/consts/prompts/lesson_topics_prompts.ts
+++ supabase/functions/_shared/consts/prompts/lesson_topics_prompts.ts
@@ -119,6 +119,7 @@ Check the length of the content is greater than 2000 words if not, add more cont
 Add any markdown formatting to the content if it improves the readability of the content. This includes, tables, lists, bold, italics, etc.
 Add emojis to the topic titles that match the topic using html dec codes.
 Ensure the length of content per topic is not too short. If so, add more content or merge topics together.
+Remove all introduction and conclusion topics and replace with more content or additional topics if necessary.
 Preserve the response structure used previously. That is, valid refined lesson content should be in this format:
 {"success":true,"data":{"topics": [{"topic":"...","content":"..."},{"topic":"...","content":"..."}]}}
 
