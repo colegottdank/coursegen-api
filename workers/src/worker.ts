579ca57df1755ba91505c2d7732ca36102ab397e
colegottdank <colegottdank@gmail.com>
2023-06-04 17:01:14 -0400

    Add cloudflare to github workflow

--- workers/src/worker.ts
+++ workers/src/worker.ts
@@ -20,15 +20,6 @@ export default {
 		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
 		const url = new URL(request.url);
 
-		// You can get pretty far with simple logic like if/switch-statements
-		switch (url.pathname) {
-			case '/redirect':
-				return handleRedirect.fetch(request, env, ctx);
-
-			case '/proxy':
-				return handleProxy.fetch(request, env, ctx);
-		}
-
 		if (url.pathname.startsWith('/api/')) {
 			// You can also use more robust routing
 			return apiRouter.handle(request);
